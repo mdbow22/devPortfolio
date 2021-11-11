674dba2692abeda888a37be338f2a000376f3379
mdbow22 <mdbow22@gmail.com>
2021-11-10 22:18:02 -0500

    layout: navbar and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.cyan,
+      red: colors.rose,
+      pink: colors.pink,
+      green: colors.lime,
+      purple: colors.purple
+    },
+    extend: {}
   },
   variants: {
     extend: {},
